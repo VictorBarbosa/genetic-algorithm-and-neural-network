9396281b129b33c32bf1ed9b84c2e4ceb079ac43
VictorBarbosa <victor.hugo.p.barbosa@hotmail.com>
2023-09-22 15:05:56 -0400

    publish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { MainComponentModule } from './folder/folder.module';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule,
 
 
-    IonicModule.forRoot(), AppRoutingModule,],
+    IonicModule.forRoot(), AppRoutingModule, ],
   providers: [
-      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-    , { provide: LocationStrategy, useClass: HashLocationStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent],
 })
